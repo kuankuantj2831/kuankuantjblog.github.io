0012c9898aaecdf567d7923ca007d71e2b204a4b
kuankuantj2831 <kuankuantj@126.com>
2025-12-15 19:59:35 +0800

    Use local Supabase SDK to fix network issues

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,5 +1,5 @@
 
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from './supabase-sdk.js'
 
 const SUPABASE_URL = 'https://rbmajbplwfyvrwtkczzn.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJibWFqYnBsd2Z5dnJ3dGtjenpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3OTY4MjksImV4cCI6MjA4MTM3MjgyOX0.QyqRAyBXe_ywsLRARu0NMnLHeN86AYAyl1C9Um9RTSE'
