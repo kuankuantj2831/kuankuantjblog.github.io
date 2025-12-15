b8e9c28ca0e8af857457f9048b4ad61c856eddd1
kuankuantj2831 <kuankuantj@126.com>
2025-12-15 20:06:21 +0800

    Fix index.html redirect and restore corrupted file

---
+++ start_server.py
@@ -0,0 +1,17 @@
+import http.server
+import socketserver
+import webbrowser
+import os
+
+PORT = 8000
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    print("Opening browser...")
+    webbrowser.open(f"http://localhost:{PORT}/index-chinese.html")
+    httpd.serve_forever()
