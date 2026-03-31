e1b7b3d69a58b6919a27ed403d112077b12cce36
kuankuantj2831 <kuankuantj@126.com>
2026-03-31 20:30:13 +0800

    移除音乐播放器弹窗的背景模糊效果

--- js/music-player.js
+++ js/music-player.js
@@ -431,7 +431,6 @@
                 right: 0;
                 bottom: 0;
                 background: rgba(0,0,0,0.6);
-                backdrop-filter: blur(4px);
             }
 
             .playlist-modal-content {
